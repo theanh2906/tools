16cccc30a8b56eee5b4ac4b8fab53fd92fcd383a
theanh2906 <theanh2906@outlook.com>
2022-10-22 00:43:51 +0700

    update

--- frontend/src/main/web/src/environments/environment.prod.ts
+++ frontend/src/main/web/src/environments/environment.prod.ts
@@ -1,4 +1,12 @@
 export const environment = {
   production: true,
   apiUrl: 'https://benna.info:8443/tools/api',
+  wsEndpoint: 'ws://benna.info:8443/tools/socket',
+  RTCPeerConfiguration: {
+    iceServers: [
+      {
+        urls: 'stun:stun1.l.google.com:19302',
+      },
+    ],
+  },
 };
